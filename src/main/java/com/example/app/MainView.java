c867f4e1a0fa5562c383d6f8c32d50a0dffaf9ef
Artur Signell <artur@vaadin.com>
2019-09-21 15:07:03 +0300

    Initial version

---
+++ src/main/java/com/example/app/MainView.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+@Tag("flex-tangle")
+@JsModule("./flex-tangle.js")
+public class MainView extends Div {
+
+}
