a024a0ba81aaf7ac6e8375ea9b62b2b1d690d465
Artur Signell <artur@vaadin.com>
2021-02-25 11:38:13 +0200

    Format svg

--- frontend/infinite-loop.ts
+++ frontend/infinite-loop.ts
@@ -63,105 +63,142 @@ export class InfiniteLoop extends LitElement {
   render() {
     return html`
       ${svg`
-      <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 125 70" version="1.1" style="height:auto">
-   <defs>
-      <path id="poly" d="m 0,10 20,10 20,-10 -20,-10 z" style="stroke:#000000;stroke-width:0.2px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1">
-      </path>
-      <clipPath id="image-clip">
-         <use href="#poly"/>
-      </clipPath>
-      <clipPath id="polys">
-         <rect x="0" y="0" width="120" height="50"/>
-      </clipPath>
-      <g id="bigglue">
-        <path d="m 0,0 40,0 -20,10 z"/>
-        <text x="17" y="4" class="glue">glue</text>
-      </g>
-      <g id="smallglue">
-        <path d="m 0,0 5,5 0,10 -5,5 z"/>
-        <text y="-2" x="7" class="glue" transform="rotate(90)" >glue</text>
-      </g>
-      <path id="fold-vertical" d="m 0,0 0,10 z" stroke-dasharray="0.3" stroke-width="0.1px"/>
-      <image href="${
-        this.image[0]
-      }" id="pic1" width="40" height="20" preserveAspectRatio="xMidYMid ${
-        this.slice[0] ? "slice" : ""
-      }" clip-path="url(#image-clip)"/>
-      <image href="${
-        this.image[1]
-      }" id="pic2" width="40" height="20" preserveAspectRatio="xMidYMid ${
-        this.slice[1] ? "slice" : ""
-      }" clip-path="url(#image-clip)"/>
-      <image href="${
-        this.image[2]
-      }" id="pic3" width="40" height="20" preserveAspectRatio="xMidYMid ${
-        this.slice[2] ? "slice" : ""
-      }" clip-path="url(#image-clip)"/>
-      <image href="${
-        this.image[3]
-      }" id="pic4" width="40" height="20" preserveAspectRatio="xMidYMid ${
-        this.slice[3] ? "slice" : ""
-      }" clip-path="url(#image-clip)"/>
-   </defs>
+        <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 125 70"
+          version="1.1"
+          style="height:auto"
+        >
+          <defs>
+            <path
+              id="poly"
+              d="m 0,10 20,10 20,-10 -20,-10 z"
+              style="stroke:#000000;stroke-width:0.2px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+            ></path>
+            <clipPath id="image-clip">
+              <use href="#poly" />
+            </clipPath>
+            <clipPath id="polys">
+              <rect x="0" y="0" width="120" height="50" />
+            </clipPath>
+            <g id="bigglue">
+              <path d="m 0,0 40,0 -20,10 z" />
+              <text x="17" y="4" class="glue">glue</text>
+            </g>
+            <g id="smallglue">
+              <path d="m 0,0 5,5 0,10 -5,5 z" />
+              <text y="-2" x="7" class="glue" transform="rotate(90)">glue</text>
+            </g>
+            <path
+              id="fold-vertical"
+              d="m 0,0 0,10 z"
+              stroke-dasharray="0.3"
+              stroke-width="0.1px"
+            />
+            <image
+              href="${this.image[0]}"
+              id="pic1"
+              width="40"
+              height="20"
+              preserveAspectRatio="xMidYMid ${this.slice[0] ? "slice" : ""}"
+              clip-path="url(#image-clip)"
+            />
+            <image
+              href="${this.image[1]}"
+              id="pic2"
+              width="40"
+              height="20"
+              preserveAspectRatio="xMidYMid ${this.slice[1] ? "slice" : ""}"
+              clip-path="url(#image-clip)"
+            />
+            <image
+              href="${this.image[2]}"
+              id="pic3"
+              width="40"
+              height="20"
+              preserveAspectRatio="xMidYMid ${this.slice[2] ? "slice" : ""}"
+              clip-path="url(#image-clip)"
+            />
+            <image
+              href="${this.image[3]}"
+              id="pic4"
+              width="40"
+              height="20"
+              preserveAspectRatio="xMidYMid ${this.slice[3] ? "slice" : ""}"
+              clip-path="url(#image-clip)"
+            />
+          </defs>
 
-   <g id="all" fill="none" style="stroke:#000000;stroke-width:0.3px">
-      <g fill="none" clip-path="url(#polys)">
-         <use href="#pic1" x="-20" y="0"/>
-         <use href="#poly" x="-20" y="0"  />
-         <use href="#pic1" x="20" y="0"/>
-         <use href="#poly" x="20" y="0" />
-         <use href="#pic1" x="60" y="0"/>
-         <use href="#poly" x="60" y="0" />
-         <use href="#pic1" x="100" y="0"/>
-         <use href="#poly" x="100" y="0" />
-         <use href="#pic2" x="0" y="10"/>
-         <use href="#poly" x="0" y="10" />
-         <use href="#pic2" x="40" y="10"/>
-         <use href="#poly" x="40" y="10" />
-         <use href="#pic2" x="80" y="10"/>
-         <use href="#poly" x="80" y="10" />
-         <use href="#pic3" x="-20" y="20"/>
-         <use href="#poly" x="-20" y="20" />
-         <use href="#pic3" x="20" y="20"/>
-         <use href="#poly" x="20" y="20" />
-         <use href="#pic3" x="60" y="20"/>
-         <use href="#poly" x="60" y="20" />
-         <use href="#pic3" x="100" y="20"/>
-         <use href="#poly" x="100" y="20" />
-         <use href="#pic4" x="0" y="30"/>
-         <use href="#poly" x="0" y="30" />
-         <use href="#pic4" x="40" y="30"/>
-         <use href="#poly" x="40" y="30" />
-         <use href="#pic4" x="80" y="30"/>
-         <use href="#poly" x="80" y="30" />
-         <path d="m 0,0 0,40" fill="red"/>
-      </g>
-      <use href="#bigglue" x="0" y="0" />
-      <use href="#bigglue" x="40" y="0" />
-      <use href="#bigglue" x="80" y="0" />
-      <use href="#smallglue" x="120" y="0" />
-      <use href="#smallglue" x="120" y="20" />
+          <g id="all" fill="none" style="stroke:#000000;stroke-width:0.3px">
+            <g fill="none" clip-path="url(#polys)">
+              <use href="#pic1" x="-20" y="0" />
+              <use href="#poly" x="-20" y="0" />
+              <use href="#pic1" x="20" y="0" />
+              <use href="#poly" x="20" y="0" />
+              <use href="#pic1" x="60" y="0" />
+              <use href="#poly" x="60" y="0" />
+              <use href="#pic1" x="100" y="0" />
+              <use href="#poly" x="100" y="0" />
+              <use href="#pic2" x="0" y="10" />
+              <use href="#poly" x="0" y="10" />
+              <use href="#pic2" x="40" y="10" />
+              <use href="#poly" x="40" y="10" />
+              <use href="#pic2" x="80" y="10" />
+              <use href="#poly" x="80" y="10" />
+              <use href="#pic3" x="-20" y="20" />
+              <use href="#poly" x="-20" y="20" />
+              <use href="#pic3" x="20" y="20" />
+              <use href="#poly" x="20" y="20" />
+              <use href="#pic3" x="60" y="20" />
+              <use href="#poly" x="60" y="20" />
+              <use href="#pic3" x="100" y="20" />
+              <use href="#poly" x="100" y="20" />
+              <use href="#pic4" x="0" y="30" />
+              <use href="#poly" x="0" y="30" />
+              <use href="#pic4" x="40" y="30" />
+              <use href="#poly" x="40" y="30" />
+              <use href="#pic4" x="80" y="30" />
+              <use href="#poly" x="80" y="30" />
+              <path d="m 0,0 0,40" fill="red" />
+            </g>
+            <use href="#bigglue" x="0" y="0" />
+            <use href="#bigglue" x="40" y="0" />
+            <use href="#bigglue" x="80" y="0" />
+            <use href="#smallglue" x="120" y="0" />
+            <use href="#smallglue" x="120" y="20" />
 
-      <use href="#fold-vertical" x="20" y="0"/>
-      <use href="#fold-vertical" x="60" y="0"/>
-      <use href="#fold-vertical" x="100" y="0"/>
-      <use href="#fold-vertical" x="20" y="50"/>
-      <use href="#fold-vertical" x="60" y="50"/>
-      <use href="#fold-vertical" x="100" y="50"/>
-      <use href="#fold-vertical" x="40" y="40"/>
-      <use href="#fold-vertical" x="80" y="40"/>
-      <use href="#fold-vertical" x="120" y="40"/>
-      <text x="10" y="55" class="instructions">
-        <tspan x="0" dy="1.2em">1. Fold and unfold along the dashed lines.</tspan>
-        <tspan x="0" dy="1.2em">2. Fold and unfold along the diagonal lines.</tspan>
-        <tspan x="0" dy="1.2em">3. Cut along the outer edge (remove the lower dashed lines)</tspan>
-        <tspan x="0" dy="1.2em">4. Fold so that two glue tabs on top align with the polygon at the bottom.</tspan>
-        <tspan x="0" dy="1.2em">5. Add glue on tabs marked with “glue” and press together. Finally glue the end tabs.</tspan>
-      </text>
-</g>
-</svg>
-`}
+            <use href="#fold-vertical" x="20" y="0" />
+            <use href="#fold-vertical" x="60" y="0" />
+            <use href="#fold-vertical" x="100" y="0" />
+            <use href="#fold-vertical" x="20" y="50" />
+            <use href="#fold-vertical" x="60" y="50" />
+            <use href="#fold-vertical" x="100" y="50" />
+            <use href="#fold-vertical" x="40" y="40" />
+            <use href="#fold-vertical" x="80" y="40" />
+            <use href="#fold-vertical" x="120" y="40" />
+            <text x="10" y="55" class="instructions">
+              <tspan x="0" dy="1.2em">
+                1. Fold and unfold along the dashed lines.
+              </tspan>
+              <tspan x="0" dy="1.2em">
+                2. Fold and unfold along the diagonal lines.
+              </tspan>
+              <tspan x="0" dy="1.2em">
+                3. Cut along the outer edge (remove the lower dashed lines)
+              </tspan>
+              <tspan x="0" dy="1.2em">
+                4. Fold so that two glue tabs on top align with the polygon at
+                the bottom.
+              </tspan>
+              <tspan x="0" dy="1.2em">
+                5. Add glue on tabs marked with “glue” and press together.
+                Finally glue the end tabs.
+              </tspan>
+            </text>
+          </g>
+        </svg>
+      `}
 ${[0, 1, 2, 3].map(
   (id) => html`
     <div class="imagecontainer">
